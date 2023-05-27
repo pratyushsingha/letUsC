b84086e19d4cce58dc069bcf5363e36e9aee2d97
pratyush Singha <pratyushsingha83@gmail.com>
2023-05-27 12:41:16 -0700

    point lies in x or y axis

--- Decision control instructions/C.j.c
+++ Decision control instructions/C.j.c
@@ -2,22 +2,22 @@
 int main()
 {
     int x1, y1, x2, y2, x3, y3;
-     printf("Enter first point(x1,y1): ");
-     scanf("%d %d", &x1, &y1);
-     printf("Enter second point(x2,y2): ");
-     scanf("%d %d", &x2, &y2);
-     printf("Enter third point(x3,y3): ");
-     scanf("%d %d", &x3, &y3);
-
-
-     int area=0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
-     if(area==0)
-     {
-         printf("Points are fall on the same straight line");
-     }
-     else
-     {
-         printf("Points are not fall on the same straight line");
-     }
-return 0;
+    printf("Enter first point(x1,y1): ");
+    scanf("%d %d", &x1, &y1);
+    printf("Enter second point(x2,y2): ");
+    scanf("%d %d", &x2, &y2);
+    printf("Enter third point(x3,y3): ");
+    scanf("%d %d", &x3, &y3);
+    
+// firstly i have to find area and then have to check if area is 0 or not to check if points are fall on the same straight line or not
+    int area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+    if (area == 0)
+    {
+        printf("Points are fall on the same straight line");
+    }
+    else
+    {
+        printf("Points are not fall on the same straight line");
+    }
+    return 0;
 }
---
+++ Decision control instructions/C.k.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x, y, r;
+     printf("Enter the coordinates(x,y) of center of a circle: ");
+     scanf("%d %d", &x, &y);
+        printf("Enter the radius of circle: ");
+        scanf("%d", &r);
+        
+return 0;
+}
