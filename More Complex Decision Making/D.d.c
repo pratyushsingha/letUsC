2c4c0e0ba707d8e68410a83e3db39459c044cf6b
pratyush Singha <pratyushsingha83@gmail.com>
2023-05-28 13:59:12 +0530

    triangle is valid or nt

---
+++ More Complex Decision Making/D.d.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+    
+    printf("Enter the value of c: ");
+    scanf("%d", &c);
+
+    if (a + b > c && a + c > b && b + c > a) {
+        printf("Triangle is valid\n");
+    } else {
+        printf("Triangle is invalid\n");
+    }
+      
+    return 0;
+}
